5b30a0e8858290d88044998d3d2cdbede4d9bd23
Tepong <yadakank@corp.ais900.org>
2016-01-07 11:47:10 +0700

    qq

--- public/js/editFunction.js
+++ public/js/editFunction.js
@@ -4,8 +4,14 @@ function PopUpDelete(funcname)
     try{
         var tag = window.confirm("Are you sure you want to delete " + funcname + " ?");
         if (tag == true) {
-            //alert("You pressed OK!");
-            window.location= funcname + "/ActionStatus/";
+            // var form = document.createElement("form");
+            // form.setAttribute("method", "post");
+            // form.setAttribute("action", "/ActionStatus/");
+            // form.submit();
+
+            document.getElementById("desForm").submit()
+            // document.getElementById("formDelete").action = "/ActionStatus/";
+            // window.location= "/ActionStatus/";
         } 
         else {
             //alert("You pressed Cancel!"); //Do nothing
@@ -16,3 +22,35 @@ function PopUpDelete(funcname)
         alert(e);
     }
 }
+
+
+// function PopUpDelete(funcname) 
+// {
+
+//     try{
+//         var popup = document.createElement("div");
+//         popup.setAttribute("data-role", "page");
+//         popup.setAttribute("data-dialog", "true");
+//         popup.setAttribute("id", "delPopup");
+
+
+
+//         if (tag == true) {
+//             // var form = document.createElement("form");
+//             // form.setAttribute("method", "post");
+//             // form.setAttribute("action", "/ActionStatus/");
+//             // form.submit();
+
+//             document.getElementById("desForm").submit()
+//             // document.getElementById("formDelete").action = "/ActionStatus/";
+//             // window.location= "/ActionStatus/";
+//         } 
+//         else {
+//             //alert("You pressed Cancel!"); //Do nothing
+//         }
+
+//     }
+//     catch(e) {
+//         alert(e);
+//     }
+// }
