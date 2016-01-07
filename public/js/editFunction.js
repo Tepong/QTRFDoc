63fe1fef54145e5ec0aaa88ca2491a7ed5ba464c
Tepong <yadakank@corp.ais900.org>
2016-01-07 10:19:02 +0700

    rr

--- public/js/editFunction.js
+++ public/js/editFunction.js
@@ -2,10 +2,10 @@ function PopUpDelete(funcname)
 {
 
     try{
-        var tag = confirm("Are you sure you want to delete " + funcname + " ?");
+        var tag = window.confirm("Are you sure you want to delete " + funcname + " ?");
         if (tag == true) {
             //alert("You pressed OK!");
-            window.location="/ActionStatus/";
+            window.location= funcname + "/ActionStatus/";
         } 
         else {
             //alert("You pressed Cancel!"); //Do nothing
