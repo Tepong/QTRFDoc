168925dbd9885a7f7233b58dfc2c97c4deca560e
MooJaa <teerapku@corp.ais900.org>
2016-01-08 17:02:53 +0700

    remove comment

--- app/controllers/funcView.controller.js
+++ app/controllers/funcView.controller.js
@@ -114,44 +114,63 @@ exports.ShowStatus = function(req, res, next){
 	});	
 };
 
-
-
 exports.UpdateData= function(req,res){
 	
 	var toUadate=req.body;
 	var conditions = { "funcname": req.params.funcName }
    	var options = { multi: false };
 	var thing = new Func(toUadate);
-   	Func.findOne({"funcname": req.params.funcName},function(err,findBefore){
-   		if(err){
-		 	return next(err);
-		 }else{
-		 	thing.save(function(err){
-		   		if(err){
-				 	return next(err);
-				 }else{
-				 	Func.remove({"_id": findBefore._id}, function(err){
-				 		if(err){
-						 	return next(err);
-						 }else{
-						 	Func.find({"projname":"SUPERSCREEN"},function(err,data){
-								if(err){
-									return next(err);
-								}else{
+
+Func.find().distinct('projname', function(err, allPr) {
+	    if(err){
+			return next(err);
+		}else{
+			Func.find({},function(err,dataAll){
+				if(err){
+					return next(err);
+				}else{
+					Func.findOne({"funcname":req.params.funcName},function(err,detail){
+						if(err){
+							return next(err);
+						}else{
+
+   							Func.findOne({"funcname": req.params.funcName},function(err,findBefore){
+   								if(err){
+		 							return next(err);
+		 						}else{
+		 							thing.save(function(err){
+		   								if(err){
+				 							return next(err);
+				 						}else{
+				 							Func.remove({"_id": findBefore._id}, function(err){
+				 								if(err){
+						 							return next(err);
+						 						}else{
+						 							Func.find({"projname":"SUPERSCREEN"},function(err,data){
+														if(err){
+															return next(err);
+													}else{
 									// res.json(toUadate);
-									res.render('funcView',{datatoview:data, funcName:req.params.funcName, statusTag:'True'});									
-								}
-								
-							});	
-						 }
-				 	});
-				 }
-			});
-		 }
-   	});
- // console.log(toUadate);
- // res.json(toUadate);
+														// res.render('funcView',{datatoview:data,funcName:req.params.funcName, statusTag:'True'});									
+														res.render('funcView',{datatoview:dataAll, allProj:allPr, dataFunc:detail,statusTag:'True'});	
+														}
+									
+													});	
+						 						}
+				 							});
+				 						}
+									});
+		 						}
+   							});
+   						}
+					});		
+				}
+			});				
+		}
+	});	
+
 };
+
 
 exports.DeleteFunction= function(req,res){
 var conditions = { "funcname": req.params.funcName, "projname":"SUPERSCREEN" }
