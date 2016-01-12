c68c441220b1c370b3762cf9b83d0b929ec35bd1
Teerapong Kruasrisawat <teerapku@corp.ais900.org>
2016-01-12 11:46:20 +0700

    eiei

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -3,16 +3,16 @@ var Func = require('../../node_modules/mongoose').model('Func');
 exports.render = function(req, res){
 	Func.find().distinct('projname', function(err, allPr) {
 	    if(err){
-				return next(err);
-			}else{
-				Func.find({},function(err,dataAll){
-					if(err){
-						return next(err);
-					}else{
-						res.render('index',{datatoview:dataAll, allProj:allPr, dataFunc:'undefined'});			
-					}
-				});				
-			}
+			return next(err);
+		}else{
+			Func.find({},function(err,dataAll){
+				if(err){
+					return next(err);
+				}else{
+					res.render('index',{datatoview:dataAll, allProj:allPr, dataFunc:'undefined'});			
+				}
+			});				
+		}
 	});	
 };
 
